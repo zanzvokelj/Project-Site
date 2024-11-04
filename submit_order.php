1b541b37e343d4b12c430d166cd4007cf61f5afd
zanzvokelj <zanzvokelj@gmail.com>
2024-11-04 17:37:26 +0100

    update2

---
+++ submit_order.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['name'];
+$contactNumber = $_POST['contactNumber'];
+$email = $_POST['email'];
+$serviceType = $_POST['serviceType'];
+$startDate = $_POST['startDate'];
+$endDate = $_POST['endDate'];
+$notes = $_POST['notes'];
+
+
+?>
